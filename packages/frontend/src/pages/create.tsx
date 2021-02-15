c6a1fcb7929f025dda3960cd0f2e70941ecb272d
ookimaki <noreply@ookimaki.com>
2021-02-16 01:49:47 +0900

    interact with contract

--- packages/frontend/src/pages/create.tsx
+++ packages/frontend/src/pages/create.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import IPFS from "ipfs-core";
 import Web3Modal from "web3modal";
-// import ethers from "ethers";
+import { ethers } from "ethers";
+// import Web3 from "web3";
+
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+import { abi as contractAbi } from "../ChocoMint.json";
 
 export const Create: React.FC = () => {
   const [ipfs, setIpfs] = React.useState<any>();
@@ -58,7 +62,20 @@ export const Create: React.FC = () => {
 
     const web3Modal = new Web3Modal();
     const web3ModalProvider = await web3Modal.connect();
+    // const web3 = new Web3(web3ModalProvider);
+    const web3Provider = new ethers.providers.Web3Provider(web3ModalProvider);
+    const signer = web3Provider.getSigner();
     console.log(web3ModalProvider, tokenUri);
+
+    const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+    const address = await signer.getAddress();
+
+    contract.mint(address, tokenUri);
+
+    // const contract = new web3.eth.Contract(contractAbi as any, contractAddress);
+    // await contract.methods
+    //   .mint("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", tokenUri)
+    //   .send({ from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" });
   };
 
   return (
