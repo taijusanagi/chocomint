6b067f3cebab421be82c55cf03caf47497d977fa
ookimaki <noreply@ookimaki.com>
2021-02-16 01:03:57 +0900

    add ipfs file uploader

--- packages/frontend/src/pages/create.tsx
+++ packages/frontend/src/pages/create.tsx
@@ -1,87 +1,92 @@
 import React from "react";
-
 import IPFS from "ipfs-core";
+import Web3Modal from "web3modal";
+// import ethers from "ethers";
 
 export const Create: React.FC = () => {
-  const initilizeIpfs = async () => {
-    const ipfs = await IPFS.create();
-    const { cid } = await ipfs.add("Hello world!!!!!");
-    console.info(cid);
-  };
+  const [ipfs, setIpfs] = React.useState<any>();
+  const [nft, setNft] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
 
   React.useEffect(() => {
-    initilizeIpfs();
+    IPFS.create().then((created) => setIpfs(created));
   }, []);
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+    const type = event.target.files[0].name.split(".")[1];
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      const buffer = new Uint8Array(reader.result as Buffer);
+      const { cid } = await ipfs.add({
+        //TODO: I don't know if this is ok
+        path: `images/nft.${type}`,
+        content: buffer,
+      });
+      setNft(`ipfs://ipfs/${cid.toString()}/nft.${type}`);
+    };
+    reader.readAsArrayBuffer(event.target.files[0]);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(event.target.value);
+  };
+
+  const uploadToIpfs = async () => {
+    const metadata = {
+      name,
+      description,
+      image: nft,
+    };
+    const buffer = Buffer.from(JSON.stringify(metadata));
+    const { cid } = await ipfs.add({
+      //TODO: I don't know if this is ok
+      path: "json/metadata.json",
+      content: buffer,
+    });
+    console.log(cid);
+    const tokenUri = `https://ipfs.io/ipfs/${cid.toString()}.metadata.json`;
+
+    const web3Modal = new Web3Modal();
+    const web3ModalProvider = await web3Modal.connect();
+    console.log(web3ModalProvider, tokenUri);
+  };
+
   return (
-    <form>
-      <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Mint Choco NFT
-      </h3>
-      <label className="block text-sm font-medium text-gray-700">
-        Upload file
-      </label>
-      <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-        <div className="space-y-1 text-center">
-          <svg
-            className="mx-auto h-12 w-12 text-gray-400"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <div className="flex text-sm text-gray-600">
-            <label
-              htmlFor="file-upload"
-              className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-            >
-              <span>Upload a file</span>
-              <input
-                id="file-upload"
-                name="file-upload"
-                type="file"
-                className="sr-only"
-              />
-            </label>
-            <p className="pl-1">or drag and drop</p>
-          </div>
-          <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-        </div>
+    <div>
+      <h3>Mint Choco NFT</h3>
+      <label>Upload file</label>
+      <input
+        type="file"
+        id="uploadedFile"
+        name="uploadedFile"
+        onChange={handleFileChange}
+      />
+      <div>
+        <label>Name</label>
+        <input type="text" name="name" id="name" onChange={handleNameChange} />
       </div>
       <div>
-        <label className="block text-sm font-medium text-gray-700">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+        <label>Description</label>
+        <textarea
+          name="description"
+          id="description"
+          onChange={handleDescriptionChange}
         />
       </div>
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Description
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-        />
-      </div>
-      <button
-        type="submit"
-        className="justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
+      <button type="submit" onClick={uploadToIpfs}>
         Mint
       </button>
-    </form>
+    </div>
   );
 };
 
