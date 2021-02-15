b936df75e9464aead2357e942b4db4c1e768e527
ookimaki <noreply@ookimaki.com>
2021-02-15 23:18:22 +0900

    add simple create

--- packages/frontend/src/components/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Test: React.FC = () => {
-  return <>Test</>;
-};
-
-export default Test;
--- packages/frontend/src/pages/create.tsx
+++ packages/frontend/src/pages/create.tsx
@@ -1,7 +1,88 @@
 import React from "react";
 
+import IPFS from "ipfs-core";
+
 export const Create: React.FC = () => {
-  return <div className="text-gray-500">create</div>;
+  const initilizeIpfs = async () => {
+    const ipfs = await IPFS.create();
+    const { cid } = await ipfs.add("Hello world!!!!!");
+    console.info(cid);
+  };
+
+  React.useEffect(() => {
+    initilizeIpfs();
+  }, []);
+
+  return (
+    <form>
+      <h3 className="text-lg leading-6 font-medium text-gray-900">
+        Mint Choco NFT
+      </h3>
+      <label className="block text-sm font-medium text-gray-700">
+        Upload file
+      </label>
+      <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+        <div className="space-y-1 text-center">
+          <svg
+            className="mx-auto h-12 w-12 text-gray-400"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <div className="flex text-sm text-gray-600">
+            <label
+              htmlFor="file-upload"
+              className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+            >
+              <span>Upload a file</span>
+              <input
+                id="file-upload"
+                name="file-upload"
+                type="file"
+                className="sr-only"
+              />
+            </label>
+            <p className="pl-1">or drag and drop</p>
+          </div>
+          <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+        </div>
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+        />
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Description
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+        />
+      </div>
+      <button
+        type="submit"
+        className="justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Mint
+      </button>
+    </form>
+  );
 };
 
 export default Create;
