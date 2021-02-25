7df19505e4753bfb178540c9061c35044352721e
ookimaki <noreply@ookimaki.com>
2021-02-25 12:53:45 +0900

    added frontend ui

--- packages/frontend/src/pages/create.tsx
+++ packages/frontend/src/pages/create.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { ethers } from "ethers";
-
-const bs58 = require("bs58");
 import {
   ipfs,
   getEthersSigner,
@@ -11,15 +8,15 @@ import {
   getContract,
 } from "../modules/web3";
 
+const bs58 = require("bs58");
 const logo = require("../assets/icon.png").default;
 
 export const Create: React.FC = () => {
   const [imageUrl, setImageUrl] = React.useState("");
-  const [imageDigest, setImageDigest] = React.useState("");
+  const [imageLoading, setImageLoading] = React.useState(false);
   const [imagePreview, setImagePreview] = React.useState("");
-  const [chainId, setChainId] = React.useState<ChainIdType>("4");
-
-  const [modalDisplay, setModalDisplay] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
 
   const readAsArrayBufferAsync = (file: File) => {
     return new Promise((resolve) => {
@@ -35,98 +32,137 @@ export const Create: React.FC = () => {
     const fileBuffer = await readAsArrayBufferAsync(file);
     const fileUint8Array = new Uint8Array(fileBuffer as Buffer);
     const { cid } = await ipfs.add(fileUint8Array);
-    return {
-      digest: `0x${bs58.decode(cid.toString()).slice(2).toString("hex")}`,
-      url: `${ipfsBaseUrl}${cid}`,
-    };
+    return `${ipfsBaseUrl}${cid}`;
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(event.target.value);
   };
 
   const handleImageChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     //This can be processed when file presents
+    setImagePreview("");
+    setImageLoading(true);
     const file = event.target.files![0];
     const preview = URL.createObjectURL(file);
+    const imageUrl = await uploadFileToIpfs(file);
     setImagePreview(preview);
-    const { url, digest } = await uploadFileToIpfs(file);
-    setImageUrl(url);
-    setImageDigest(digest);
-    setModalDisplay(true);
+    setImageUrl(imageUrl);
+    setImageLoading(false);
   };
 
   const createNft = async () => {
+    console.log("test");
     const signer = await getEthersSigner();
-    const chainId = await (await signer.getChainId()).toString();
-    if (chainId != "4" && chainId != "80001") {
+    const chainId = await signer.getChainId();
+    if (chainId != 4 && chainId != 80001) {
       alert("Please connect to Rinkeby or Matic Testnet.");
       return;
     }
-
-    const iss = await signer.getAddress();
-    const { contractAddress } = getNetworkConfig(chainId as ChainIdType);
-    const choco = {
-      chainId,
-      contractAddress: contractAddress.toLowerCase(),
-      tokenId: "", //calculated later
-      name: "",
-      image: imageUrl,
-      iss: iss.toLowerCase(),
-    };
-
-    const messageHash = ethers.utils.solidityKeccak256(
-      ["uint256", "address", "bytes32", "address"],
-      [chainId, choco.contractAddress, imageDigest, choco.iss]
+    const { contractAddress } = getNetworkConfig(
+      chainId.toString() as ChainIdType
     );
-    choco.tokenId = ethers.BigNumber.from(messageHash).toString();
-    const imageCid = imageUrl.split("://")[1];
-    choco.name = `NFT#${imageCid}`;
+    const choco = {
+      name: "choco",
+      description: "this is chocomint",
+      image: imageUrl,
+    };
     const metadataString = JSON.stringify(choco);
-
     const { cid } = await ipfs.add(metadataString);
-    console.log(cid.toString());
-    console.log(metadataString);
-
-    const contract = getContract(choco.contractAddress).connect(signer);
-
-    await contract.mint(imageDigest);
-    console.log("tokenId", choco.tokenId);
+    const contract = getContract(contractAddress).connect(signer);
+    const digest = `0x${bs58.decode(cid.toString()).slice(2).toString("hex")}`;
+    const hash = await contract.mint(digest);
+    console.log("hash", hash);
   };
 
   return (
-    <div className="container mx-auto h-screen bg-white p-40">
-      <div>
-        <div className="mx-auto w-full">
-          <img className="mx-auto h-12 w-auto" src={logo} alt="logo" />
-          <h2 className="mt-2 text-center text-3xl font-extrabold text-gray-900">
-            Let’s create NFTs!
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600 max-w">
-            <a
-              href="https://chocomint.app"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              Decentralized multi-chain NFTs publish platform
+    <div className="container mx-auto h-screen bg-white">
+      <div className="flex justify-center">
+        <div className="w-full max-w-md p-4">
+          <div>
+            <a href="https://chocomint.app">
+              <img
+                className="mx-auto h-20 rounded-xl w-auto border-b-4 border-green-700 shadow-md"
+                src={logo}
+                alt="logo"
+              />
             </a>
-          </p>
-        </div>
-        <div className="mt-8">
-          <div className="mt-1 sm:mt-0 sm:col-span-2 mx-auto">
-            <div className="flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-              <div className="space-y-1 text-center">
-                <svg
-                  className="mx-auto h-12 w-12 text-gray-400"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 48 48"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+            <h2 className="mt-2 text-center text-3xl font-extrabold text-gray-900">
+              Let’s create NFTs!
+            </h2>
+          </div>
+          <div className="mt-2">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Name
+            </label>
+            <input
+              onChange={handleNameChange}
+              type="text"
+              name="name"
+              id="name"
+              autoComplete="given-name"
+              className="mt-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-xl"
+            />
+          </div>
+          <div className="mt-2">
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Description
+            </label>
+            <textarea
+              onChange={handleDescriptionChange}
+              id="description"
+              name="description"
+              className="mt-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-xl"
+            ></textarea>
+          </div>
+
+          <div className="mt-2">
+            <label
+              htmlFor="cover_photo"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Image
+            </label>
+
+            <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-xl">
+              <div className="space-y-4 text-center">
+                {!imagePreview ? (
+                  <svg
+                    className={`mx-auto h-20 w-20 text-gray-400 ${
+                      imageLoading && "animate-bounce"
+                    }`}
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                ) : (
+                  <img
+                    className="mx-auto h-20 w-20 rounded-xl border-b-4 border-green-700 shadow-md"
+                    src={imagePreview}
                   />
-                </svg>
+                )}
                 <div className="flex text-sm text-gray-600">
                   <label
                     htmlFor="file"
@@ -134,14 +170,14 @@ export const Create: React.FC = () => {
                   >
                     <span>Upload a file</span>
                     <input
+                      onChange={handleImageChange}
                       id="file"
                       name="file"
                       type="file"
                       className="sr-only"
-                      onChange={handleImageChange}
                     />
                   </label>
-                  <p className="pl-1">or drag and drop</p>
+                  <p className="pl-1">to ipfs. It takes some time.</p>
                 </div>
                 <p className="text-xs text-gray-500">
                   PNG, JPG, GIF up to 10MB
@@ -149,37 +185,16 @@ export const Create: React.FC = () => {
               </div>
             </div>
           </div>
-        </div>
-        {modalDisplay && (
-          <div className="absolute fixed z-20 inset-0">
-            <div className="flex min-h-screen p-4">
-              <div className="fixed inset-0">
-                <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
-              </div>
-              <div className="flex bg-white rounded-lg w-full mx-auto shadow-2xl transform">
-                <div className="flex flex-col h-full w-full justify-between">
-                  <h2 className="mt-8 text-center text-3xl font-extrabold text-gray-900">
-                    Your NFT!
-                  </h2>
-                  <div className="w-full p-4">
-                    <img
-                      className="w-full mx-auto rounded-xl"
-                      src={imagePreview}
-                    />
-                  </div>
-                  <div className="p-8">
-                    <button
-                      onClick={createNft}
-                      className="w-full bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded"
-                    >
-                      Mint
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="mt-12">
+            <button
+              onClick={createNft}
+              disabled={!name || !description || !imageUrl}
+              className="disabled:opacity-50 w-full rounded-xl max-w-md bg-green-500 text-white font-bold py-2 px-4 border-b-4 border-green-700 rounded shadow-2xl"
+            >
+              Mint
+            </button>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
