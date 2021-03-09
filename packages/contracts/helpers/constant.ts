2e1c662e8a3020e8c415540c51f56185f2c2ca7d
ookimaki <noreply@ookimaki.com>
2021-03-09 15:29:36 +0900

    modified prices

--- packages/contracts/helpers/constant.ts
+++ packages/contracts/helpers/constant.ts
@@ -1,8 +1,8 @@
 export const nullAddress = "0x0000000000000000000000000000000000000000";
 
 export const BASE_RATIO = "10000";
-export const defaultSupplyLimit = "256";
-export const defaultVirtualSupply = "100";
+export const defaultSupplyLimit = "128";
+export const defaultVirtualSupply = "64";
 export const defaultVirtualReserve = "10000000000000000";
 export const defaultRoyalityRatio = "1000";
 export const defaultCrr = "1000";
--- packages/contracts/helpers/mock.ts
+++ packages/contracts/helpers/mock.ts
@@ -3,7 +3,14 @@ export const dummyMetadataIpfsHash =
   "0x7d5a99f603f231d53a4f39d1521f98d2e8bb279cf29bebfd0687dc98458e7f89";
 export const hardhatChainId = "31337";
 
-export const expectedDefaultPriceForFirstPrint = "10000000000000000";
-export const expectedDefaultPriceForSecondPrint = "10792079207920000";
-export const expectedDefaultPriceForThirdPrint = "11638516792850000";
-export const expectedDefaultPriceForForthPrint = "12542479262200000";
+// this is mock value for prices
+// export const BASE_RATIO = "10000";
+// export const defaultSupplyLimit = "128";
+// export const defaultVirtualSupply = "64";
+// export const defaultVirtualReserve = "10000000000000000";
+// export const defaultRoyalityRatio = "1000";
+// export const defaultCrr = "1000";
+
+export const expectedDefaultPriceForFirstPrint = "1562500000000000";
+export const expectedDefaultPriceForSecondPrint = "1754807692300000";
+export const expectedDefaultPriceForThirdPrint = "1967511655010000";
