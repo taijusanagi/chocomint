235b266e1f2b7c5a24729ae35eb498926dfa22b7
kb_windows <kusakabob@gmail.com>
2021-03-11 19:25:29 +0900

    font-fixed-include

--- packages/frontend/src/components/organisms/Footer.tsx
+++ packages/frontend/src/components/organisms/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="text-center font-medium p-4 text-xs text-gray-400">
+    <footer className="text-center font-medium p-4 text-xs text-gray-400 font-way">
       <Link to="/">@chocomint</Link>
     </footer>
   );
