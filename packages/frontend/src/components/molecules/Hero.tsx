82299d340c19c851419eddbcb50d593a5abc8308
ookimaki <noreply@ookimaki.com>
2021-03-11 12:07:00 +0900

    added docs in hero

--- packages/frontend/src/components/molecules/Hero.tsx
+++ packages/frontend/src/components/molecules/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "../atoms/Button";
 
 export interface HeroProps {
@@ -7,21 +7,19 @@ export interface HeroProps {
 }
 
 export const Hero: React.FC<HeroProps> = ({ src }) => {
-  const history = useHistory();
-  const onClick = () => {
-    history.push("/create");
-  };
-
   return (
     <section>
       <div className="w-full">
         <div className="flex flex-col items-center mx-auto bg-gradient-to-r from-green-400 via-blue-100 to-green-400 p-8">
           <img className="max-w-96 max-h-80 mt-6 mb-3" src={src} />
           <div className="mx-auto py-4">
-            <div className="flex items-center justify-center sm:px-1">
-              <Button type="tertiary" onClick={onClick}>
-                Create NFT
-              </Button>
+            <div className="flex items-center justify-center sm:px-1 space-x-4">
+              <Link to="/create" className="w-32">
+                <Button type="primary">CreateNFT</Button>
+              </Link>
+              <a href="https://docs.chocomint.app" className="w-32">
+                <Button type="tertiary">Docs</Button>
+              </a>
             </div>
           </div>
         </div>
