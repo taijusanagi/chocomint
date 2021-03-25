2d2e57a5e56fa01bac2b7ee8da3ef0cd85aa9ca8
Daiki Kunii <>
2021-03-25 11:39:41 +0900

    Update the structure of Config.

--- packages/frontend/src/modules/firebase.tsx
+++ packages/frontend/src/modules/firebase.tsx
@@ -3,22 +3,37 @@ import firebase from "firebase";
 import firebaseJson from "../../../../firebase.json";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDThONNzesmZYFpnXvIoSBTn6eIm69I4tQ",
-  authDomain: "chocomint-prod.firebaseapp.com",
-  projectId: "chocomint-prod",
-  storageBucket: "chocomint-prod.appspot.com",
-  messagingSenderId: "464874047282",
-  appId: "1:464874047282:web:323cf0fc34b53c2e6ab92b",
-  measurementId: "G-VZ4DF3ZNTH",
+  // MEMO: Here we don't want to affect the production's analytics.
+  production: {
+    apiKey: "AIzaSyDThONNzesmZYFpnXvIoSBTn6eIm69I4tQ",
+    authDomain: "chocomint-prod.firebaseapp.com",
+    projectId: "chocomint-prod",
+    storageBucket: "chocomint-prod.appspot.com",
+    messagingSenderId: "464874047282",
+    appId: "1:464874047282:web:323cf0fc34b53c2e6ab92b",
+    measurementId: "G-VZ4DF3ZNTH",
+  },
+  development: {
+    apiKey: "AIzaSyDThONNzesmZYFpnXvIoSBTn6eIm69I4tQ",
+    authDomain: "chocomint-prod.firebaseapp.com",
+    projectId: "chocomint-prod",
+    storageBucket: "chocomint-prod.appspot.com",
+    messagingSenderId: "464874047282",
+    appId: "1:464874047282:web:323cf0fc34b53c2e6ab92b",
+    measurementId: "G-54TRPXY0KK", // MEMO: Temporary measurement ID
+  },
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+const nodeEnv = process.env.NODE_ENV;
+const app = firebase.initializeApp(
+  firebaseConfig[nodeEnv === "production" ? "production" : "development"]
+);
 
 const analytics = app.analytics();
 const firestore = app.firestore();
 const functions = app.functions();
 
-if (process.env.NODE_ENV === "development") {
+if (nodeEnv === "development") {
   firestore.settings({
     host: `localhost:${firebaseJson.emulators.firestore.port}`,
     ssl: false,
